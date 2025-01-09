a9a6047a6745dfccb0559ac25a07ec0cf21209c0
Jazmin <giljazmin2005@gmail.com>
2025-01-08 18:58:57 -0600

    Tarea 7

--- Tareas/Tesla.py
+++ Tareas/Tesla.py
@@ -1,18 +1,18 @@
 
 def es_numero_tesla(numero):
-    # Reducir el número a un solo dígito
-    while numero >= 10:
-        suma = 0
-        while numero > 0:
-            suma += numero % 10  # Sumar el último dígito
-            numero //= 10        # Quitar el último dígito
+    
+    while numero >= 10: #Ensures that a number is processed if it is more than digit long.
+        suma = 0 #Initializes the variable where the sum is accumulated.
+        while numero > 0: #Processes the number digit by digit.
+            suma += numero % 10  # Sum the last digit.
+            numero //= 10        #Remove the last digit.
         numero = suma
 
-    # Comprobar si el dígito final es 3, 6 o 9
+    # Check if the last digit is 3, 6 o 9
     return numero in [3, 6, 9]
 
-# Prueba del programa
-numero = int(input("Ingresa un número: "))
+
+numero = int(input("Ingresa un número: ")) #Asks the user for a number.
 if es_numero_tesla(numero):
     print("Es un número Tesla.")
 else:
