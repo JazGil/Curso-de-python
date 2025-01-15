31920a2d8f1b2e4b68b7f97b730f6e624b9d8a0d
Jazmin <giljazmin2005@gmail.com>
2025-01-15 07:19:33 -0600

    Print

---
+++ Tareas/Tarea6.py
@@ -0,0 +1,15 @@
+
+print("'¡Hola's Mundo!'") #print: '¡Hola's Mundo!'
+
+#print(' " ¡Hola's Mundo!" ') The single quotation mark of (Hola's) generate a confusion, Pythons interprets it as an or end of the string.
+
+print(' "¡Hola\'s Mundo!" ') #To keep the single quotation mark, you can put a single slash, to "escape" tha quotation mark inside the string. 
+
+print( " ¡Hola's Mundo!" ) #If i remove both single quotes it prints:  ¡Hola's Mundo!
+
+print( " '¡Holas Mundo!' " ) #print:  '¡Holas Mundo!'
+
+print(" ¡Hola's Mundo!'") #print: ¡Hola's Mundo!'
+
+print("'¡Hola's Mundo!") #print: '¡Hola's Mundo!
+
